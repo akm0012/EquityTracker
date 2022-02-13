59b05fad3c69e8474dcea49c5e650a31bd0ea2c8
akm0012 <akm0012@auburn.edu>
2022-02-13 16:07:42 -0500

    Wrote some tests

--- prod/repository/ConfigRepository.py
+++ prod/repository/ConfigRepository.py
@@ -10,11 +10,16 @@ class ConfigRepository:
 
     config_file_location = ""
 
+    API_TOKENS_SECTION = "API_TOKENS"
+    DATABASE_SECTION = "DATABASE"
+    NO_KEY = "NO_KEY"
+
     def __init__(self):
         script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
         rel_path = "../../config.ini"
         self.config_file_location = os.path.join(script_dir, rel_path)
 
+    # todo delete soon
     def read_ini(self, file_path: str):
         config = configparser.ConfigParser()
         config.read(file_path)
@@ -35,9 +40,14 @@ class ConfigRepository:
         config.read(self.config_file_location)
         if config.sections().__len__() != 2:
             return False
-        if not config.has_section("API_TOKENS"):
+        if not config.has_section(self.API_TOKENS_SECTION):
             return False
-        if not config.has_section("DATABASE"):
+        if not config.has_section(self.DATABASE_SECTION):
+            return False
+
+        # Make sure there is an API Key
+        api_tokens_key = config.get('API_TOKENS', 'FINN_HUB_API_KEY', fallback=self.NO_KEY)
+        if api_tokens_key == self.NO_KEY:
             return False
 
         return True
---
+++ test/repository/test_ConfigRepository.py
@@ -0,0 +1,73 @@
+import os
+from unittest import TestCase
+
+from prod.repository.ConfigRepository import ConfigRepository
+
+
+class TestConfigRepository(TestCase):
+
+    # region does_config_file_exist tests
+
+    def test_does_config_file_exist_true(self):
+        config_repo = ConfigRepository()
+        config_repo.config_file_location = self.__get_path_to_test_file("test_config_1.ini")
+        self.assertTrue(config_repo.does_config_file_exist())
+
+    def test_does_config_file_exist_false(self):
+        config_repo = ConfigRepository()
+        config_repo.config_file_location = self.__get_path_to_test_file("not_a_file.ini")
+        self.assertFalse(config_repo.does_config_file_exist())
+
+    # endregion
+
+    # region is_config_file_valid tests
+
+    def test_is_config_file_valid_no_file_found(self):
+        config_repo = ConfigRepository()
+        config_repo.config_file_location = self.__get_path_to_test_file("not_a_file.ini")
+        try:
+            config_repo.is_config_file_valid()
+        except FileNotFoundError as myException:
+            self.assertTrue(myException.args[0].startswith("Config file can not be found."))
+
+    def test_is_config_file_valid_no_sections(self):
+        config_repo = ConfigRepository()
+        config_repo.config_file_location = self.__get_path_to_test_file("test_config_2.ini")
+        self.assertFalse(config_repo.is_config_file_valid())
+
+    def test_is_config_file_valid_too_many_sections(self):
+        config_repo = ConfigRepository()
+        config_repo.config_file_location = self.__get_path_to_test_file("test_config_3.ini")
+        self.assertFalse(config_repo.is_config_file_valid())
+
+    def test_is_config_file_valid_no_api_token(self):
+        config_repo = ConfigRepository()
+        config_repo.config_file_location = self.__get_path_to_test_file("test_config_4.ini")
+        self.assertFalse(config_repo.is_config_file_valid())
+
+    def test_is_config_file_valid_no_database(self):
+        config_repo = ConfigRepository()
+        config_repo.config_file_location = self.__get_path_to_test_file("test_config_5.ini")
+        self.assertFalse(config_repo.is_config_file_valid())
+
+    def test_is_config_file_valid_no_api_token_value(self):
+        config_repo = ConfigRepository()
+        config_repo.config_file_location = self.__get_path_to_test_file("test_config_6.ini")
+        self.assertFalse(config_repo.is_config_file_valid())
+
+    def test_is_config_file_valid_true_1(self):
+        config_repo = ConfigRepository()
+        config_repo.config_file_location = self.__get_path_to_test_file("test_config_1.ini")
+        self.assertTrue(config_repo.is_config_file_valid())
+
+    def test_is_config_file_valid_true_2(self):
+        config_repo = ConfigRepository()
+        config_repo.config_file_location = self.__get_path_to_test_file("test_config_7.ini")
+        self.assertTrue(config_repo.is_config_file_valid())
+
+    # endregion
+
+    def __get_path_to_test_file(self, test_file_name: str) -> str:
+        script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
+        rel_path = "../test_config_files/" + test_file_name
+        return os.path.join(script_dir, rel_path)
