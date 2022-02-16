a4c4378091a4762ff959e2eea87df8f0a4524a3d
akm0012 <akm0012@auburn.edu>
2022-02-16 12:40:02 -0500

    Added more math functions

--- prod/util/MathUtil.py
+++ prod/util/MathUtil.py
@@ -34,6 +34,22 @@ def calculate_grant_percent_change(current_stock_price: float,
     percent_change = calculate_percent_change(original_grant_price, current_grant_price)
     return round(percent_change, 2)
 
+"""
+Calculates the dollar difference of a grant and it's current stock price. 
+IE: current_stock_price = $60.00
+    grant_price = $50.00
+    shares = 2000
+
+    Dollar Difference = $20,000
+"""
+def calculate_grant_dollar_change(current_stock_price: float,
+                                    num_of_shares: int,
+                                    grant_share_price: float) -> float:
+    current_grant_price = current_stock_price * num_of_shares
+    original_grant_price = grant_share_price * num_of_shares
+
+    dollar_change = current_grant_price - original_grant_price
+    return round(dollar_change, 2)
 
 """
 Calculates the percent difference of a list of grants and it's current stock price. 
@@ -60,3 +76,30 @@ def calculate_multi_grant_percent_change(current_stock_price: float,
 
     total_percent_change = calculate_percent_change(original_total_grant_price, current_total_grant_price)
     return round(total_percent_change, 2)
+
+
+"""
+Calculates the dollar difference of a list of grants and it's current stock price. 
+IE: current_stock_price = $60.00
+
+    grant_1 = StockGrant("TWTR", 2000, 50)
+    grant_2 = StockGrant("TWTR", 100, 70)
+
+    Dollar Difference = $19,000
+"""
+def calculate_multi_grant_dollar_change(current_stock_price: float,
+                                         stock_grant_list: list[StockGrant]):
+    # Make sure all grants are for the same stock
+    ticker = stock_grant_list[0].ticker
+    for stock in stock_grant_list:
+        if stock.ticker != ticker:
+            raise ValueError("All Stocks must have the same Ticker.")
+
+    original_total_grant_price = 0
+    current_total_grant_price = 0
+    for stock in stock_grant_list:
+        original_total_grant_price += stock.price * stock.count
+        current_total_grant_price += current_stock_price * stock.count
+
+    total_dollar_change = current_total_grant_price - original_total_grant_price
+    return round(total_dollar_change, 2)
--- test/util/test_MathUtil.py
+++ test/util/test_MathUtil.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 from prod.objects.StockGrant import StockGrant
 from prod.util.MathUtil import calculate_percent_change, calculate_grant_percent_change, \
-    calculate_multi_grant_percent_change
+    calculate_multi_grant_percent_change, calculate_multi_grant_dollar_change, calculate_grant_dollar_change
 
 
 class MathUtilTests(TestCase):
@@ -43,6 +43,28 @@ class MathUtilTests(TestCase):
                                                 grant_price)
         self.assertEqual(expected, actual)
 
+    def test_calculate_grant_dollar_change(self):
+        current_share_price = 60.00
+        grant_price = 50.00
+        shares = 2000
+
+        expected = 20000
+        actual = calculate_grant_dollar_change(current_share_price,
+                                                shares,
+                                                grant_price)
+        self.assertEqual(expected, actual)
+
+    def test_calculate_grant_dollar_change(self):
+        current_share_price = 60.00
+        grant_price = 70.00
+        shares = 100
+
+        expected = -1000
+        actual = calculate_grant_dollar_change(current_share_price,
+                                                shares,
+                                                grant_price)
+        self.assertEqual(expected, actual)
+
     def test_calculate_multi_grant_percent_change(self):
         current_share_price = 60.00
 
@@ -51,5 +73,16 @@ class MathUtilTests(TestCase):
 
         expected = 17.76
         actual = calculate_multi_grant_percent_change(current_share_price, [grant_1, grant_2])
+
+        self.assertEqual(expected, actual)
+
+    def test_calculate_multi_grant_dollar_change(self):
+        current_share_price = 60.00
+
+        grant_1 = StockGrant("TWTR", 2000, 50)
+        grant_2 = StockGrant("TWTR", 100, 70)
+
+        expected = 19000
+        actual = calculate_multi_grant_dollar_change(current_share_price, [grant_1, grant_2])
 
         self.assertEqual(expected, actual)
